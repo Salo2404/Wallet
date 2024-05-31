33bf0fa93c32e1c9eb03f1acdc291474454f9143
Artur <122271936+Salo2404@users.noreply.github.com>
2024-05-31 16:24:53 +0500

    Add files via upload

---
+++ Менеджер финансов заготовка/main.js
@@ -0,0 +1,12 @@
+const addButton = document.querySelector('.btn');
+const editor = document.querySelector('.editor__form-input');
+const selectCategory = document.querySelector('#priceInput');
+const tbody = document.querySelector('#tbody');
+
+addButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    let newProduct = document.createElement('tr')
+    newProduct.classList
+
+
+})
